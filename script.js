a673cb8522609e196146dde3677882c98ec29871
Fernando Peres <75857732+Reff20@users.noreply.github.com>
2021-01-05 19:12:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('button')
+btn.addEventListener('click' , () => {
+    console.log("teste")
+    window.location.href = 'https://www.linkedin.com/in/fernando-peres-carvalho-25b4661b7/'
+})
